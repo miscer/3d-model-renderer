3f8c99f4384698535f1ff06c05c02d1ca7297f17
Michal Miškerník <michal@miskernik.sk>
2018-04-24 21:14:23 +0100

    Refactoring

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -10,50 +10,19 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     public Canvas canvas;
-    private GraphicsContext context;
+    private Renderer renderer;
 
     private static final Camera CAMERA = new Camera(1200, 800, 240000, 160000);
-
-    private static final SimpleMatrix LIGHT = Vectors.create3d(0, 0, -20000);
+    private static final SimpleMatrix LIGHT = Vectors.create3d(0, 0, 0);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        context = canvas.getGraphicsContext2D();
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        renderer = new Renderer(context);
     }
 
     public void render(List<Triangle> triangles) {
-        SimpleMatrix camera = Vectors.create3d(0, 0, -20000);
-
-        triangles.sort((o1, o2) -> -Double.compare(
-                Vectors.distance(o2.getCenteroid(), camera),
-                Vectors.distance(o1.getCenteroid(), camera)
-        ));
-
-        for (Triangle triangle : triangles) {
-            renderTriangle(triangle);
-        }
-    }
-
-    private void renderTriangle(Triangle triangle) {
-        SimpleMatrix m = triangle.getMatrix();
-        SimpleMatrix a = CAMERA.project(m.extractVector(false, 0));
-        SimpleMatrix b = CAMERA.project(m.extractVector(false, 1));
-        SimpleMatrix c = CAMERA.project(m.extractVector(false, 2));
-
-        context.beginPath();
-        context.moveTo(a.get(0), a.get(1));
-        context.lineTo(b.get(0), b.get(1));
-        context.lineTo(c.get(0), c.get(1));
-        context.closePath();
-
-        double illumination = Vectors.dot(
-                Vectors.unit(triangle.getSurfaceNormal()),
-                Vectors.unit(LIGHT.minus(triangle.getCenteroid()))
-        );
-
-        Color fill = Color.gray(Math.abs(illumination));
-        context.setFill(fill);
-
-        context.fill();
+        Renderer.prepareTriangles(triangles);
+        renderer.renderTriangles(CAMERA, LIGHT, triangles);
     }
 }
---
+++ src/main/java/Renderer.java
@@ -0,0 +1,56 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.List;
+
+public class Renderer {
+    private GraphicsContext context;
+
+    public Renderer(GraphicsContext context) {
+        this.context = context;
+    }
+
+    public static void prepareTriangles(List<Triangle> triangles) {
+        SimpleMatrix camera = Vectors.create3d(0, 0, -20000);
+
+        triangles.sort((o1, o2) -> -Double.compare(
+                Vectors.distance(o2.getCenteroid(), camera),
+                Vectors.distance(o1.getCenteroid(), camera)
+        ));
+    }
+
+    public void renderTriangles(Camera camera, SimpleMatrix light, List<Triangle> triangles) {
+        for (Triangle triangle : triangles) {
+            renderTriangle(camera, light, triangle);
+        }
+    }
+
+    public void renderTriangle(Camera camera, SimpleMatrix light, Triangle triangle) {
+        SimpleMatrix m = triangle.getMatrix();
+        SimpleMatrix a = camera.project(m.extractVector(false, 0));
+        SimpleMatrix b = camera.project(m.extractVector(false, 1));
+        SimpleMatrix c = camera.project(m.extractVector(false, 2));
+
+        context.beginPath();
+        context.moveTo(a.get(0), a.get(1));
+        context.lineTo(b.get(0), b.get(1));
+        context.lineTo(c.get(0), c.get(1));
+        context.closePath();
+
+        Color fill = getTriangleColor(light, triangle);
+        context.setFill(fill);
+
+        context.fill();
+    }
+
+    private Color getTriangleColor(SimpleMatrix light, Triangle triangle) {
+        double illumination = Vectors.dot(
+                Vectors.unit(triangle.getSurfaceNormal()),
+                Vectors.unit(light.minus(triangle.getCenteroid()))
+        );
+
+        return Color.gray(Math.max(illumination, 0));
+    }
+
+}
