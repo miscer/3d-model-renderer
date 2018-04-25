4a4b0d41e1b7fe98d47a3a740bbe10d383375ace
Michal Miškerník <michal@miskernik.sk>
2018-04-25 21:21:06 +0100

    Controlling light position and intensity

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,6 +1,8 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.List;
@@ -8,19 +10,38 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     public Canvas canvas;
+    public TextField lightPositionX;
+    public TextField lightPositionY;
+    public TextField lightPositionZ;
+    public TextField lightIntensity;
     private Renderer renderer;
-
-    private static final Camera CAMERA = new Camera(1200, 800, 240000, 160000);
-    private static final Light LIGHT = new Light(1, Vectors.create3d(0, 0, 0));
+    private List<Triangle> triangles;
+    private Camera camera;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         GraphicsContext context = canvas.getGraphicsContext2D();
+
         renderer = new Renderer(context);
+        camera = new Camera(canvas.getWidth(), canvas.getHeight(), 240000, 160000);
     }
 
     public void render(List<Triangle> triangles) {
         Renderer.prepareTriangles(triangles);
-        renderer.renderTriangles(CAMERA, LIGHT, triangles);
+        this.triangles = triangles;
+
+        renderer.renderTriangles(camera, getLight(), triangles);
+    }
+
+    public void handleRenderClick(ActionEvent actionEvent) {
+        renderer.renderTriangles(camera, getLight(), triangles);
+    }
+
+    private Light getLight() {
+        double x = Double.parseDouble(lightPositionX.getText());
+        double y = Double.parseDouble(lightPositionY.getText());
+        double z = Double.parseDouble(lightPositionZ.getText());
+        double intensity = Double.parseDouble(lightIntensity.getText());
+        return new Light(intensity, Vectors.create3d(x, y, z));
     }
 }
