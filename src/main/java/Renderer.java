55a47a07b1b028c8398913a7d31ccf77e98837cf
Michal Miškerník <michal@miskernik.sk>
2018-04-26 22:28:19 +0100

    Comments

--- src/main/java/Renderer.java
+++ src/main/java/Renderer.java
@@ -13,6 +13,7 @@ public class Renderer {
 
     /**
      * Creates a new renderer that will use the specified context for rendering a model
+     *
      * @param context Canvas graphics context
      */
     public Renderer(GraphicsContext context) {
@@ -22,6 +23,7 @@ public class Renderer {
 
     /**
      * Prepares the list of triangles to be rendered; orders them by the z-coordinate of their centeroids
+     *
      * @param triangles
      */
     public static void prepareTriangles(List<Triangle> triangles) {
@@ -38,8 +40,9 @@ public class Renderer {
 
     /**
      * Renders all triangles onto the canvas
-     * @param camera Camera used for projection
-     * @param light Light used for illumination
+     *
+     * @param camera    Camera used for projection
+     * @param light     Light used for illumination
      * @param triangles Rendered model
      */
     public void renderTriangles(Camera camera, Light light, List<Triangle> triangles) {
@@ -64,28 +67,41 @@ public class Renderer {
     }
 
     private void fillTriangle(SimpleMatrix a, SimpleMatrix b, SimpleMatrix c, SimpleMatrix texture) {
-        SimpleMatrix dp1 = b.minus(a);
-        SimpleMatrix dp2 = c.minus(a);
+        // calculate the number of samples (the step size)
+        double step = getTriangleFillStep(a, b, c);
 
-        double dc1 = texture.get(1) - texture.get(0);
-        double dc2 = texture.get(2) - texture.get(0);
+        // sample values of j and k in the range [0, 1], such that j + k <= 1
+        for (double j = 0; j <= 1; j += step) {
+            for (double k = 0; k <= 1; k += step) {
+                if (j + k <= 1) {
+                    // calculate value of i based on j and k
+                    double i = 1 - j - k;
 
-        for (double i = 0; i <= 1; i += 0.1) {
-            for (double j = 0; j <= 1; j += 0.1) {
-                if (i + j <= 1) {
-                    SimpleMatrix p = a.plus(dp1.scale(i)).plus(dp2.scale(j));
+                    // interpolate the position and colour of the point
+                    SimpleMatrix position = a.scale(i).plus(b.scale(j)).plus(c.scale(k));
+                    double color = texture.get(0) * i + texture.get(1) * j + texture.get(2) * k;
 
-                    double gray = Math.min(Math.max(texture.get(0) + i * dc1 + j * dc2, 0), 255);
-                    Color color = Color.gray(gray / 255);
-
+                    // paint the pixel
                     writer.setColor(
-                            (int) p.get(0),
-                            (int) p.get(1),
-                            color
+                            (int) Math.round(position.get(0)),
+                            (int) Math.round(position.get(1)),
+                            Color.gray(color / 255)
                     );
                 }
             }
         }
     }
 
+    private double getTriangleFillStep(SimpleMatrix a, SimpleMatrix b, SimpleMatrix c) {
+        // calculate the bounding box of the triangle
+        double maxX = Math.max(a.get(0), Math.max(b.get(0), c.get(0)));
+        double minX = Math.min(a.get(0), Math.max(b.get(0), c.get(0)));
+        double maxY = Math.max(a.get(1), Math.max(b.get(1), c.get(1)));
+        double minY = Math.min(a.get(1), Math.max(b.get(1), c.get(1)));
+
+        // select either width or height, depending on which one is bigger
+        double maxDelta = Math.max(maxX - minX, maxY - minY);
+
+        return 1 / maxDelta;
+    }
 }
